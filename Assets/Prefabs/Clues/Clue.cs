741b86518d8841f834f65d764f4a938c34122d43
celloguy <will_wright24@yahoo.com>
2019-12-09 12:32:19 -0600

    Added the stuff that was causing merge conflicts without potential clues

--- Assets/Prefabs/Clues/Clue.cs
+++ Assets/Prefabs/Clues/Clue.cs
@@ -5,42 +5,59 @@ using UnityEngine;
 public class Clue : MonoBehaviour
 {
     //Clue script is going to handle responding to a user when they are nearby
-    SphereCollider collider;
     // Start is called before the first frame update
-    void Start()
-    {
-        collider = gameObject.GetComponent<SphereCollider>();
-        collider.enabled = false;
-    }
-
+    GameObject go;
+    Light clueLight;
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Player has entered a clue zone");
+        clueLight.enabled = true;
         //Make the clue more visible
     }
 
     private void OnTriggerExit(Collider other)
     {
+        Debug.Log("Player has exited a clue zone");
+        clueLight.enabled = false;
         //Make the clue less visible
     }
 
     public void findClue()
     {
+        Debug.Log("Player has found the clue");
         //Do the thing to count this clue as found
-        collider.enabled = false;
+        GetComponent<Collider>().enabled = false;
+        Destroy(go);
+        Destroy(gameObject);
+        //coll.enabled = false;
         //Make the clue less visible
     }
 
-    public bool setRealClue()
+    public bool setRealClue(GameObject go)
     {
-        if (collider.enabled)
+        Debug.Log(gameObject + " is now a clue at: " + transform.position);
+        //if (gameObject.name != null)
+        //    return true;
+        //transform.parent.gameObject
+        if (GetComponent<Collider>().enabled)
             return false;
-        collider.enabled = true;
+        this.go = Instantiate(go, transform.parent, false);
+        clueLight = this.go.GetComponent<Light>();
+        clueLight.enabled = false;
+        GetComponent<Collider>().enabled = true;
         return true;
+    }
+
+
+    public void destroyFakes()
+    {
+        if (go == null)
+            Destroy(gameObject);
     }
 }
