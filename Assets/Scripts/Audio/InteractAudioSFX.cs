b9786e9d98a9acf5c33368e30a3f4a22ccbe3e6a
Jared Andrzejewski <35735389+JaredAndrz@users.noreply.github.com>
2019-12-09 01:25:06 -0600

    Added Audio Sources And Scripts for Basic Implementation

---
+++ Assets/Scripts/Audio/InteractAudioSFX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractAudioSFX : MonoBehaviour
+{
+    public static AudioSource sfx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sfx = GetComponent<AudioSource>();
+    }
+    
+    public static void playSFX()
+    {
+        sfx.Play();  
+    }
+}
