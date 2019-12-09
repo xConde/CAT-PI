a6c46a3f6a2a0ad40f9be46425fe2ceabb8442be
Jared Andrzejewski <35735389+JaredAndrz@users.noreply.github.com>
2019-12-09 01:49:29 -0600

    Audio (#7)
    * Added Audio Sources And Scripts for Basic Implementation
    
    * Added Sound Options To Some Scripts

---
+++ Assets/Scripts/Audio/MenuSelectSFX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSelectSFX : MonoBehaviour
+{
+    public static AudioSource sfx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sfx = GetComponent<AudioSource>();
+    }
+    
+    public static void playSFX()
+    {
+        sfx.Play();  
+    }
+}
