2fec024c65dc8b95530d526bd18766ebf77011f4
celloguy <will_wright24@yahoo.com>
2019-12-08 21:37:30 -0600

    Adding the Clue objects

---
+++ Assets/Prefabs/Clues/Clue.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clue : MonoBehaviour
+{
+    //Clue script is going to handle responding to a user when they are nearby
+    SphereCollider collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = gameObject.GetComponent<SphereCollider>();
+        collider.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Make the clue more visible
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Make the clue less visible
+    }
+
+    public void findClue()
+    {
+        //Do the thing to count this clue as found
+        collider.enabled = false;
+        //Make the clue less visible
+    }
+
+    public bool setRealClue()
+    {
+        if (collider.enabled)
+            return false;
+        collider.enabled = true;
+        return true;
+    }
+}
---
+++ Assets/Prefabs/Clues/Clues.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clues : MonoBehaviour
+{
+    private int _cluesFound;
+    private string[] clues =
+    {
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    };
+
+    private Clue[] _potentialClue;
+    private List<Clue> _realClues;
+    public string getClue
+    {
+        get {
+            return clues[_cluesFound];
+        }
+        set {
+            _cluesFound++;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get List of all clues
+        _potentialClue = GameObject.FindObjectsOfType<Clue>();
+        _realClues = new List<Clue>();
+        //Mark objects as clues
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void chooseClues(int numClues)
+    {
+        //int[] randomClueNums = new int[numClues];
+        int i = 0;
+        while(i< numClues)
+        {
+            int r = Random.Range(0,_potentialClue.Length);
+            if (_potentialClue[r].setRealClue())
+            {
+                _realClues.Add(_potentialClue[r]);
+                i++;
+
+            }
+        }
+    }
+}
